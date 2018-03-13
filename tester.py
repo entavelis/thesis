0a98165ffa6f4688d7ff22fd8acef6b76e152a8a
VglsD <vaggelis@fsu.gr>
2018-03-13 22:07:13 +0100

    Text Preprocessing

--- tester.py
+++ tester.py
@@ -1,6 +1,8 @@
 import onmt.Models as models
 import onmt.ModelConstructor as con
 
+import onmt
+
 import opts
 
 import json
@@ -17,27 +19,44 @@ spacy_en = spacy.load('en')
 def tokenizer(text): # create a tokenizer function
     return [tok.text for tok in spacy_en.tokenizer(text)]
 
-cap = dset.CocoCaptions(root = '/media/ctrlaltv/USBitch/MSCOCO/val2014',
-                        annFile = '/media/ctrlaltv/USBitch/MSCOCO/annotations/captions_val2014.json',
-                        transform=transforms.ToTensor())
-
-print(len(cap))
+# cap = dset.CocoCaptions(root = '/media/ctrlaltv/USBitch/MSCOCO/val2014',
+#                         annFile = '/media/ctrlaltv/USBitch/MSCOCO/annotations/captions_val2014.json',
+#                         transform=transforms.ToTensor())
+2
+# print(len(cap))
 # for i,(img, txt) in enumerate(cap):
 
 # annotations = "/media/ctrlaltv/USBitch/MSCOCO/annotations/"
-# val = json.load(open(annotations + 'captions_val2014.json', 'r'))
-# train = json.load(open(annotations + 'captions_train2014.json', 'r'))
+annotations = "annotations/"
+val = json.load(open(annotations + 'captions_val2014.json', 'r'))
+train = json.load(open(annotations + 'captions_train2014.json', 'r'))
 
 # val = data.TabularDataset(
 #        path=(annotations + 'captions_val2014.json'), format='json',
 #        fields={'annotations': ('caption',  data.Field(sequential=True, tokenize=tokenizer, lower=True))})
 
 vocab = set([])
-for annot in val["annotations"]:
-    cap = dict(annot)["caption"]
-    vocab.update(set(cap.lower().rstrip("\n").rstrip(".").rstrip(",").split(" ")))
+with open("data/valid.src.txt", 'wb') as f:
+    for annot in val["annotations"]:
+        cap = dict(annot)["caption"]
+    # vocab.update(set(cap.lower().rstrip("\n").rstrip(".").rstrip(",").split(" ")))
+        f.write(" ".join(tokenizer(cap)) + "\n")
 
-# print(len(vocab))
+with open("data/train.src.txt", 'wb') as f:
+    for annot in train["annotations"]:
+        cap = dict(annot)["caption"]
+        f.writelines(" ".join(tokenizer(cap)) + "\n")
+    # vocab.update(set(cap.lower().rstrip("\n").rstrip(".").rstrip(",").split(" ")))
+
+    # vocab.update(set(tokenizer(cap)))
+
+# vocab_size = len(vocab)
+# word_to_ix = {word: i for i, word in enumerate(vocab)}
+#
+
+
+
+
 # for cap in val.fields.iteritems():
 #        print(cap)
 
