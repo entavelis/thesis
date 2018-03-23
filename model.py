b11f8742c5501e3d97e6324ca125e9a885647a02
VglsD <vaggelis@fsu.gr>
2018-03-23 12:29:37 +0100

    Preparing Model

--- model.py
+++ model.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -11,6 +5,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import ipdb
 
+from onmt import Models
 import numpy as np
 
 kernel_sizes = [4,3,3]
@@ -23,16 +18,59 @@ latent_dim = 300
 class TextEncoder(nn.Module):
     def __init__(
             self,
-            embedding_dim
+            embeddings
             ):
         super(TextEncoder, self).__init__()
         
-        self.hidden_dim = latent_dim
-        self
-        
-        
-    
-    
+        self.hidden_dim = embedding_dim
+
+        rnn_type = "GRU"
+        brnn = True
+        enc_layers = 100
+        rnn_size = latent_dim
+        dropout = 0.3
+        bridge = True
+        self.encoder = Models.RNNEncoder(rnn_type, brnn, enc_layers,
+                          rnn_size, dropout, embeddings,
+                          bridge)
+
+    def forward(self, src, lengths):
+
+        # tgt = tgt[:-1]  # exclude last target from inputs
+        return self.encoder(src, lengths)
+
+class TextDecoder(nn.Module):
+    def __init__(
+            self,
+            embeddings
+            ):
+
+        rnn_type = "GRU"
+        brnn = True
+        dec_layers =2
+        rnn_size = latent_dim
+        global_attention = True
+        coverage_attn = True
+        context_gate = True
+        copy_attn = True
+        dropout = 0.3
+        reuse_copy_attn = True
+
+
+        self.decoder = Models.StdRNNDecoder(rnn_type, brnn,
+                             dec_layers, rnn_size,
+                             global_attention,
+                             coverage_attn,
+                             context_gate,
+                             copy_attn,
+                             dropout,
+                             embeddings,
+                             reuse_copy_attn)
+
+
+
+
+
 class ImageEncoder(nn.Module):
     def __init__(
             self,
@@ -125,12 +163,49 @@ class ImageDecoder(nn.Module):
         return self.main( input )
 
     
-class TextDecoder(nn.Module):
-    
 class ImageDiscriminator(nn.Module):
+    def __init__(
+            self,
+            ):
 
-class TextDiscriminator(nn.Module):
-        
+        super(ImageDiscriminator, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, 4, 2, 1, bias=False)
+        self.relu1 = nn.LeakyReLU(0.2, inplace=True)
+
+        self.conv2 = nn.Conv2d(64, 64 * 2, 4, 2, 1, bias=False)
+        self.bn2 = nn.BatchNorm2d(64 * 2)
+        self.relu2 = nn.LeakyReLU(0.2, inplace=True)
+
+        self.conv3 = nn.Conv2d(64 * 2, 64 * 4, 4, 2, 1, bias=False)
+        self.bn3 = nn.BatchNorm2d(64 * 4)
+        self.relu3 = nn.LeakyReLU(0.2, inplace=True)
+
+        self.conv4 = nn.Conv2d(64 * 4, 64 * 8, 4, 2, 1, bias=False)
+        self.bn4 = nn.BatchNorm2d(64 * 8)
+        self.relu4 = nn.LeakyReLU(0.2, inplace=True)
+
+        self.conv5 = nn.Conv2d(64 * 8, 1, 4, 1, 0, bias=False)
+
+    def forward(self, input):
+        conv1 = self.conv1( input )
+        relu1 = self.relu1( conv1 )
+
+        conv2 = self.conv2( relu1 )
+        bn2 = self.bn2( conv2 )
+        relu2 = self.relu2( bn2 )
+
+        conv3 = self.conv3( relu2 )
+        bn3 = self.bn3( conv3 )
+        relu3 = self.relu3( bn3 )
+
+        conv4 = self.conv4( relu3 )
+        bn4 = self.bn4( conv4 )
+        relu4 = self.relu4( bn4 )
+
+        conv5 = self.conv5( relu4 )
+
+        return torch.sigmoid( conv5 ), [relu2, relu3, relu4]
+
 class Discriminator(nn.Module):
     def __init__(
             self,
